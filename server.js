6099b291dd211b0bff21dc19a44b0caeafe94df7
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-12 04:30:45 -0500

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
    client: 'pg',
    connection: {
-     host : '127.0.0.1',
+     host : 'postgresql-shaped-77245',
      user : 'postgres',
      password : 'test',
      database : 'facerecognition-brain'
