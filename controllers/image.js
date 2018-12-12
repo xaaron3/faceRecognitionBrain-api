d699707b06b7c7db092483d2e15c8a39e12c2cc1
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-12 02:12:43 -0500

    moving the clarifai api use into this repo from the front end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'f0f7558592a7428b91be217eeab74239'
+ });
+
+ const handleApiCall = (req, res) => {
+   app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+ }
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +25,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
