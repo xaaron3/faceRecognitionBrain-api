40a5004f11dbf38f6caaf854ac38bfc4a221b936
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-12 01:57:01 -0500

    fixes empty user registration and validations

--- server.js
+++ server.js
@@ -24,15 +24,10 @@ app.use(bodyParser.json())    // use after const=app has been created.
 app.use(cors())
 
 app.get('/', (req, res)=> {res.send(database.users)})
-
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 app.listen(3000, ()=> {
    console.log('app is running test, on port 3000')
