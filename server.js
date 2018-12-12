1ccc4f3792b56f7b4c850d46efff00d83d42ae96
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-12 03:59:22 -0500

    changing response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json())    // use after const=app has been created. 
 app.use(cors())
 
-app.get('/', (req, res)=> {res.send(database.users)})
+app.get('/', (req, res)=> {res.send('it is working!')})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
